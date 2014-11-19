edd4256f1d83f10b925b8a7ed2ff61dbcd5f48c3
kirang89 <kiran.daredevil@gmail.com>
2014-11-19 08:02:39 +0530

    Add few settings; Setup nose

--- kiran/init-python.el
+++ kiran/init-python.el
@@ -7,6 +7,8 @@
 (add-hook 'python-mode-hook 'yas-minor-mode)
 (add-hook 'python-mode-hook 'auto-complete-mode)
 
+(add-hook 'py-shell-hook '(lambda () (linum-mode -1)))
+
 ;; Access symbol lookup using C-h S
 (require 'pydoc-info)
 
@@ -22,21 +24,38 @@
             (local-set-key (kbd "M-.") 'jedi:goto-definition)))
 
 ;;setting up iPython as interpreter
-(setq
- python-shell-interpreter "ipython"
- python-shell-interpreter-args ""
- python-shell-prompt-regexp "In \\[[0-9]+\\]: "
- python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
- python-shell-completion-setup-code
-   "from IPython.core.completerlib import module_completion"
- python-shell-completion-module-string-code
-   "';'.join(module_completion('''%s'''))\n"
- python-shell-completion-string-code
-   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+;;(require 'ipython)
+;; (setq
+;;  python-shell-interpreter "ipython"
+;;  python-shell-interpreter-args ""
+;;  python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+;;  python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+;;  python-shell-completion-setup-code
+;;    "from IPython.core.completerlib import module_completion"
+;;  python-shell-completion-module-string-code
+;;    "';'.join(module_completion('''%s'''))\n"
+;;  python-shell-completion-string-code
+;;    "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+;; (setq py-force-py-shell-name-p t)
+
+;; ;; switch to the interpreter after executing code
+;; (setq py-shell-switch-buffers-on-execute-p t)
+;; (setq py-switch-buffers-on-execute-p t)
+;; ;; don't split windows
+;; (setq py-split-windows-on-execute-p nil)
+;; ;; try to automagically figure out indentation
+;; (setq py-smart-indentation t)
+
 
 ;; flycheck support
 (require 'flycheck)
 (add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; Make python shell use utf-8 encoding
+(setenv "LC_CTYPE" "UTF-8")
+
+(require 'nose)
+(defvar nose-use-verbose nil)
 
 (provide 'init-python)
 ;;; init-python.el ends here
