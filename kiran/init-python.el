8a5d4b4c1c7dd7bc95cf8417944edb96f6b1303b
Kiran Gangadharan <kiran@Kirans-MacBook-Pro.local>
2015-03-22 12:22:28 +0530

    Few modifications to python settings

--- kiran/init-python.el
+++ kiran/init-python.el
@@ -3,10 +3,13 @@
 (require 'python-mode)
 (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
 (setq py-electric-colon-active t)
-(add-hook 'python-mode-hook 'autopair-mode)
+
+; Set PYTHONPATH, because we don't load .bashrc
+(setenv "PYTHONPATH" "/usr/local/lib/python2.7/site-packages:")
+
+;(add-hook 'python-mode-hook 'autopair-mode)
 (add-hook 'python-mode-hook 'yas-minor-mode)
 (add-hook 'python-mode-hook 'auto-complete-mode)
-
 (add-hook 'py-shell-hook '(lambda () (linum-mode -1)))
 
 ;; Access symbol lookup using C-h S
@@ -20,24 +23,27 @@
 	    (jedi:setup)
 	    (jedi:ac-setup)
             (local-set-key "\C-cd" 'jedi:show-doc)
-            (local-set-key (kbd "M-SPC") 'jedi:complete)
+            ;(local-set-key (kbd "M-SPC") 'jedi:complete)
             (local-set-key (kbd "M-.") 'jedi:goto-definition)))
 
 ;;setting up iPython as interpreter
 ;;(require 'ipython)
-;; (setq
-;;  python-shell-interpreter "ipython"
-;;  python-shell-interpreter-args ""
-;;  python-shell-prompt-regexp "In \\[[0-9]+\\]: "
-;;  python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
-;;  python-shell-completion-setup-code
-;;    "from IPython.core.completerlib import module_completion"
-;;  python-shell-completion-module-string-code
-;;    "';'.join(module_completion('''%s'''))\n"
-;;  python-shell-completion-string-code
-;;    "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
-;; (setq py-force-py-shell-name-p t)
+(setq
+ python-shell-interpreter "ipython"
+ python-shell-interpreter-args "--colors=Linux"
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code
+   "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+   "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
 
+;;Fix for NameError when launching ipython
+(setq py-python-command-args '("--colors=linux"))
+
+;; (setq py-force-py-shell-name-p t)
 ;; ;; switch to the interpreter after executing code
 ;; (setq py-shell-switch-buffers-on-execute-p t)
 ;; (setq py-switch-buffers-on-execute-p t)
