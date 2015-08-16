f370393facffe1cf0517a2f15803f180398f9d49
kirang89 <me@kirang.in>
2015-08-16 23:39:46 +0530

    Minor cleanup

--- init.el
+++ init.el
@@ -209,14 +209,16 @@
 (require 'helm-config)
 (helm-mode 1)
 (setq helm-buffers-fuzzy-matching t)
-(global-set-key (kbd "C-x b")   #'helm-mini)
+
+;; (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+(global-set-key (kbd "C-x b") #'helm-mini)
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "M-y") #'helm-show-kill-ring)
 ;; (global-set-key (kbd "C-x C-b") #'helm-buffers-list)
-(global-set-key (kbd "C-x C-m") #'helm-M-x)
 ;; (global-set-key (kbd "C-x C-f") #'helm-find-files)
 ;; (global-set-key (kbd "C-x C-r") #'helm-recentf)
-(global-set-key (kbd "M-y") #'helm-show-kill-ring)
 
-;;; =====================================
+;;; =================================================================
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
