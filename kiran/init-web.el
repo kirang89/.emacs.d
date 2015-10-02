eb248ccbd4e99d44de575731128a92caae58733f
kirang89 <me@kirang.in>
2015-10-02 07:29:22 +0530

    Add emmet mode configuration to web mode init

--- kiran/init-web.el
+++ kiran/init-web.el
@@ -9,5 +9,9 @@
 (setq web-mode-enable-auto-expanding t)
 (setq web-mode-enable-css-colorization t)
 
+(require 'emmet-mode)
+(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+(add-hook 'html-mode-hook 'emmet-mode)
+(add-hook 'css-mode-hook  'emmet-mode)
 
 (provide 'init-web)
