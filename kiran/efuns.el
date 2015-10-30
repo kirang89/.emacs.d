45637d82dd2f68f730738895a6bdddeef86528d5
kirang89 <me@kirang.in>
2015-10-30 09:14:00 +0530

    Add elisp function to drag files into org buffer

--- kiran/efuns.el
+++ kiran/efuns.el
@@ -167,5 +167,47 @@ Requires 'pcsv' package to be installed."
 
 (global-set-key (kbd "C-a") 'beginning-of-line-dwim)
 
+(defun kg/drag-n-drop (event)
+  "Drag files onto org-mode and insert a link to them"
+  (interactive "e")
+  (goto-char (nth 1 (event-start event)))
+  (x-focus-frame nil)
+  (let* ((payload (car (last event)))
+         (type (car payload))
+         (fname (cadr payload))
+         (img-regexp "\\(png\\|jp[e]?g\\)\\>"))
+    (cond
+     ;; insert image link
+     ((and  (eq 'drag-n-drop (car event))
+            (eq 'file type)
+            (string-match img-regexp fname))
+      (insert (format "[[%s]]" fname))
+      (org-display-inline-images t t))
+     ;; insert image link with caption
+     ((and  (eq 'C-drag-n-drop (car event))
+            (eq 'file type)
+            (string-match img-regexp fname))
+      (insert "#+ATTR_ORG: :width 300\n")
+      (insert (concat  "#+CAPTION: " (read-input "Caption: ") "\n"))
+      (insert (format "[[%s]]" fname))
+      (org-display-inline-images t t))
+     ;; C-drag-n-drop to open a file
+     ((and  (eq 'C-drag-n-drop (car event))
+            (eq 'file type))
+      (find-file fname))
+     ((and (eq 'M-drag-n-drop (car event))
+           (eq 'file type))
+      (insert (format "[[attachfile:%s]]" fname)))
+     ;; regular drag and drop on file
+     ((eq 'file type)
+      (insert (format "[[%s]]\n" fname)))
+     (t
+      (error "I am not equipped for dnd on %s" payload)))))
+
+;; (define-key org-mode-map (kbd "<drag-n-drop>") 'kg/drag-n-drop)
+;; (define-key org-mode-map (kbd "<C-drag-n-drop>") 'kg/drag-n-drop)
+;; (define-key org-mode-map (kbd "<M-drag-n-drop>") 'kg/drag-n-drop)
+
+
 (provide 'efuns)
 ;;; efuns.el ends here
