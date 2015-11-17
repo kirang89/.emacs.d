6f790821a3eef7cebb694a37b06ba31ae259d92e
kirang89 <me@kirang.in>
2015-11-17 23:25:24 +0530

    Set terminal and kbd coding system to UTF-8

--- init.el
+++ init.el
@@ -7,6 +7,8 @@
 (setq current-language-environment "English")
 
 ;; Prefer unicode encoding everywhere
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 (setq default-buffer-file-coding-system 'utf-8)
 
