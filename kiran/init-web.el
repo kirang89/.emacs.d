1126f7db7d3a0b08a118528753957b317832bf15
kirang89 <me@kirang.in>
2015-11-25 22:53:50 +0530

    Add/Modify configuration for web-mode

--- kiran/init-web.el
+++ kiran/init-web.el
@@ -1,6 +1,10 @@
+;;;
+;;; Configuration for editing html, js and css
+;;;
 
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css?\\'" . web-mode))
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
@@ -10,8 +14,8 @@
 (setq web-mode-enable-css-colorization t)
 
 (require 'emmet-mode)
+(add-hook 'web-mode-hook 'emmet-mode)
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
-(add-hook 'html-mode-hook 'emmet-mode)
-(add-hook 'css-mode-hook  'emmet-mode)
 
 (provide 'init-web)
+;;; init-web.el ends here
