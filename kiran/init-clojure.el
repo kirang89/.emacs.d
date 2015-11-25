e1766e7de77234a4bd4c1b3c3cee5c79383722d9
kirang89 <me@kirang.in>
2015-11-25 22:43:28 +0530

    Minor cleanups to deal with the annoying warning

--- kiran/init-clojure.el
+++ kiran/init-clojure.el
@@ -14,9 +14,10 @@
 ;; A little more syntax highlighting
 (require 'clojure-mode-extra-font-locking)
 
-;; syntax hilighting for midje
+;; syntax highlighting in repl
 (add-hook 'clojure-mode-hook
           (lambda ()
+            (defvar inferior-lisp-program)
             (setq inferior-lisp-program "lein repl")
             (font-lock-add-keywords
              nil
@@ -27,6 +28,9 @@
             (define-clojure-indent (fact 1))
             (define-clojure-indent (facts 1))))
 
+(add-hook 'clojure-mode-hook
+          (lambda () (local-set-key (kbd "C-c C-s") 'paredit-wrap-round)))
+
 ;;;;
 ;; Cider
 ;;;;
@@ -35,23 +39,29 @@
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 
 ;; go right to the REPL buffer when it's finished connecting
+(defvar cider-repl-pop-to-buffer-on-connect)
 (setq cider-repl-pop-to-buffer-on-connect t)
 
 ;; When there's a cider error, show its buffer and switch to it
+(defvar cider-show-error-buffer)
 (setq cider-show-error-buffer t)
+
+(defvar cider-auto-select-error-buffer)
 (setq cider-auto-select-error-buffer t)
 
 ;; Where to store the cider history.
+(defvar cider-repl-history-file)
 (setq cider-repl-history-file "~/.emacs.d/cider-history")
 
-;; Wrap when navigating history.
+;; Wrap when navigating history
+(defvar cider-repl-wrap-history)
 (setq cider-repl-wrap-history t)
+
+(defvar cider-repl-use-clojure-font-lock)
+(setq cider-repl-use-clojure-font-lock t)
 
 ;; enable paredit in your REPL
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
-
-(add-hook 'clojure-mode-hook
-          (lambda () (local-set-key (kbd "C-c C-s") 'paredit-wrap-round)))
 
 (provide 'init-clojure)
 ;;; init-clojure.el ends here
