9a293f36db260d6a7b8432ff02348a7a3e27b11f
kirang89 <me@kirang.in>
2015-11-25 22:45:20 +0530

    Add keybinding for helm-recentf

--- kiran/init-helm.el
+++ kiran/init-helm.el
@@ -1,5 +1,4 @@
 ;; Helm configuration
-;; Author: Kiran Gangadharan
 
 (require 'helm-config)
 (helm-mode 1)
@@ -14,8 +13,9 @@
 (global-set-key (kbd "C-x b") #'helm-mini)
 (global-set-key (kbd "M-x") #'helm-M-x)
 (global-set-key (kbd "M-y") #'helm-show-kill-ring)
+(global-set-key (kbd "C-x C-r") #'helm-recentf)
 ;; (global-set-key (kbd "C-x C-b") #'helm-buffers-list)
 ;; (global-set-key (kbd "C-x C-f") #'helm-find-files)
-;; (global-set-key (kbd "C-x C-r") #'helm-recentf)
 
 (provide 'init-helm)
+;;; init-helm.el ends here
