53d92961f1944e4b77c59e625e98d62d5f670182
kirang89 <me@kirang.in>
2015-12-14 17:38:38 +0400

    Add seperate config file for elisp mode

---
+++ kiran/init-elisp.el
@@ -0,0 +1,17 @@
+;;; init-elisp --- Configuration for Emacs Lisp mode-line
+;;
+;;; Commentary:
+;;
+;; Configuration for Emacs Lisp mode-line
+;;
+;;; Code:
+
+(add-hook 'emacs-lisp-mode-hook 'flycheck-mode)
+(add-hook 'emacs-lisp-mode-hook 'linum-mode)
+;; Documentation at point for lisp files
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'emacs-lisp-mode-hook (lambda () (rainbow-delimiters-mode)))
+
+(provide 'init-elisp)
+;;; init-elisp ends here
