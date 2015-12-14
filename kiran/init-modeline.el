3a1dbcbc41a523bd33c33d91f7610d5c31178d8a
kirang89 <me@kirang.in>
2015-12-14 17:40:22 +0400

    Use Abbreviations/Pretty Symbols for certain major modes

--- kiran/init-modeline.el
+++ kiran/init-modeline.el
@@ -48,5 +48,31 @@
 (defadvice emacs-lisp-mode (after elisp-rename-modeline activate)
   (setq mode-name "ELisp"))
 
+(defvar mode-line-cleaner-alist
+  `((paredit-mode . " π")
+    ;; Major modes
+    (hi-lock-mode . "")
+    (python-mode . "Py")
+    (clojure-mode . "λ"))
+  "Alist for `clean-mode-line'.
+
+When you add a new element to the alist, keep in mind that you
+must pass the correct minor/major mode symbol and a string you
+want to use in the modeline *in lieu of* the original.")
+
+(defun clean-mode-line ()
+  (interactive)
+  (loop for cleaner in mode-line-cleaner-alist
+        do (let* ((mode (car cleaner))
+                 (mode-str (cdr cleaner))
+                 (old-mode-str (cdr (assq mode minor-mode-alist))))
+             (when old-mode-str
+                 (setcar old-mode-str mode-str))
+               ;; major mode
+             (when (eq mode major-mode)
+               (setq mode-name mode-str)))))
+
+(add-hook 'after-change-major-mode-hook 'clean-mode-line)
+
 (provide 'init-modeline)
 ;;; init-modeline.el ends here
