95693849130e8d09042cf552db092bf43fe70e8b
kirang89 <me@kirang.in>
2016-02-15 19:51:02 +0530

    Get org-html export css from an external css file

--- kiran/init-org.el
+++ kiran/init-org.el
@@ -29,6 +29,8 @@
 
 ;; Make code pretty
 (setq org-src-fontify-natively t)
+;; change behavior of TAB as per the appropriate major mode
+(setq org-src-tab-acts-natively t)
 
 ;; Spellcheck my org mode files.
 (add-hook 'org-mode-hook 'flyspell-mode)
@@ -54,21 +56,30 @@
 ;; Remove html postamble
 (setq org-html-postamble nil)
 
-;; Add to the exported html's header
-(setq org-html-head-extra "
-   <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
-   <link href='http://fonts.googleapis.com/css?family=Source+Code+Pro:400,700' rel='stylesheet' type='text/css'>
-   <style type='text/css'>
-      body {
-         font-family: 'Source Sans Pro', sans-serif;
-         padding-left: '60px';
-         padding-right: '60px';
-      }
-      pre, code {
-         font-family: 'Source Code Pro', monospace;
-      }
-   </style>")
 
+(defun get-string-from-file (filePath)
+  "Return filePath's file content."
+  (with-temp-buffer
+    (insert-file-contents filePath)
+    (buffer-string)))
+
+(defvar html-export-headers "
+   <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+   <link href='http://fonts.googleapis.com/css?family=Source+Code+Pro:400,700' rel='stylesheet' type='text/css'>")
+
+(defvar html-export-styles nil
+  (concat "<style type='text/css'>"
+          (get-string-from-file "~/.emacs.d/kiran/htmlexport.css")
+          "</style>"))
+
+;; value of a defvar does not override by default
+(setq html-export-styles (concat "<style type='text/css'>"
+          (get-string-from-file "~/.emacs.d/kiran/htmlexport.css")
+          "</style>"))
+
+;; Add to the exported html's header
+(setq org-html-head-extra (concat html-export-headers
+                                  html-export-styles))
 
 ;; Activate babel support for specified languages
 (org-babel-do-load-languages
