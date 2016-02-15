f9676121f12edaf1c53ba63c1bcc2f2f7a77ac17
kirang89 <me@kirang.in>
2016-02-15 19:46:13 +0530

    Add few settings for clojure mode

--- kiran/init-clojure.el
+++ kiran/init-clojure.el
@@ -9,7 +9,7 @@
 ;; Java classes (e.g. JavaClassName)
 (add-hook 'clojure-mode-hook 'subword-mode)
 
-(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+;; (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
 
 ;; A little more syntax highlighting
 (require 'clojure-mode-extra-font-locking)
@@ -17,7 +17,6 @@
 ;; syntax highlighting in repl
 (add-hook 'clojure-mode-hook
           (lambda ()
-            (defvar inferior-lisp-program)
             (setq inferior-lisp-program "lein repl")
             (font-lock-add-keywords
              nil
@@ -31,6 +30,7 @@
 
 
 (add-hook 'clojure-mode-hook 'flycheck-mode)
+(add-hook 'clojure-mode-hook 'linum-mode)
 
 (add-hook 'clojure-mode-hook
           (lambda () (local-set-key (kbd "C-c C-s") 'paredit-wrap-round)))
@@ -45,13 +45,16 @@
       cider-auto-select-error-buffer t
       cider-repl-history-file "~/.emacs.d/cider-history"
       cider-repl-wrap-history t
-      cider-repl-use-clojure-font-lock t)
+      cider-repl-use-clojure-font-lock t
+      cider-docview-fill-column 70
+      cider-stacktrace-fill-column 76
+      cider-repl-use-pretty-printing t)
 
 ;; provides minibuffer documentation for the code you're typing into the repl
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 ;; enable paredit in REPL
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
-(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
+;; (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
 
 (defun kg/clj-src-file-name-from-test (name)
   (s-with name
@@ -62,6 +65,19 @@
   (s-with name
     (s-replace "/src/" "/test/")
     (s-replace ".clj" "_test.clj")))
+
+(defun kg/set-pretty-symbols ()
+  "make some word or string show as pretty Unicode symbols"
+  (setq prettify-symbols-alist
+        '(
+          ("fn"  . 955)     ; λ
+          ("->"  . 8594)    ; →
+          ("=>"  . 8658)    ; ⇒
+          ("map" . 8614)    ; ↦
+          )))
+
+(add-hook 'clojure-mode-hook 'kg/set-pretty-symbols)
+(add-hook 'clojure-mode-hook 'paren-face-mode)
 
 (provide 'init-clojure)
 ;;; init-clojure.el ends here
