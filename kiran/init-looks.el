97377e2db5722a09d4248b948f53df3398d0a835
kirang89 <me@kirang.in>
2016-02-15 19:47:19 +0530

    Minor changes to init-looks.el

--- kiran/init-looks.el
+++ kiran/init-looks.el
@@ -40,7 +40,7 @@
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
 
 ; Minimal fringe
-(fringe-mode 0)
+(fringe-mode 1)
 
 ;; Experimental
 (defun kg/set-fringe-background ()
@@ -108,6 +108,25 @@
 (window-numbering-mode t)
 
 (global-prettify-symbols-mode 1)
+
+;; (defun kg/reset-frame-look ()
+;;   (interactive)
+;;   (set-face-attribute 'linum nil :font "Fira Code 10")
+;;   (fringe-mode 0)
+;;   (powerline-reset)
+;;   (let ((faces '(mode-line
+;;                mode-line-buffer-id
+;;                mode-line-emphasis
+;;                mode-line-highlight
+;;                mode-line-inactive)))
+;;      (mapc
+;;       (lambda (face)
+;;         (set-face-attribute face nil :font "Fira Code 11"))
+;;       faces)))
+
+;; Make Emacs handle sRGB colors correctly
+;; for solarized theme
+;; (setq solarized-broken-srgb nil)
 
 (provide 'init-looks)
 ;;; init-looks.el ends here
