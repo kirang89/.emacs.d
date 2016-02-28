3b2bef6830145f32d50aa1a08320a4b46d2a8117
kirang89 <me@kirang.in>
2016-02-28 22:31:10 +0530

    Move ibuffer config into a new file

---
+++ kiran/init-ibuffer.el
@@ -0,0 +1,44 @@
+
+
+;; IBuffer
+(global-set-key (kbd "C-x C-b") #'ibuffer)
+
+
+;; Gnus style grouping for ibuffer
+;; Check http://www.emacswiki.org/emacs/IbufferMode
+(setq ibuffer-saved-filter-groups
+      (quote (("default"
+               ("PYTHON" (or
+                          (mode . python-mode)
+                          (mode . elpy-mode)))
+               ("CLOJURE" (or
+                           (mode . clojure-mode)
+                           (mode . cider-mode)
+                           (mode . clojurec-mode)
+                           (name . ".cljc$")
+                           (name . "^\\*cider")
+                           (name . "^\\*nrepl")))
+               ("ORG" (mode . org-mode))
+               ("DIRED" (mode . dired-mode))
+               ("MARKDOWN" (mode . markdown-mode))
+               ("WEBDEV" (mode . web-mode))
+               ("MAGIT" (or
+                         (mode . magit-mode)
+                         (name . "^\\*magit")))
+               ("PLANNER" (or
+                           (name . "^\\*Calendar\\*$")
+                           (name . "^diary$")
+                           (mode . muse-mode)))
+               ("EMACS" (or
+                         (name . "^\\*scratch\\*$")
+                         (name . "^\\*Messages\\*$")))
+               ("HELM" (or
+                        (mode . helm-mode)
+                        (name . "^\\*helm")))))))
+
+(add-hook 'ibuffer-mode-hook
+              (lambda ()
+                (ibuffer-switch-to-saved-filter-groups "default")))
+
+(provide 'init-ibuffer)
+;;; init-ibuffer.el ends here
