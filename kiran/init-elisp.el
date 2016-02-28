ab8fb5f6ea7fe1a1a1fdf2fde25afaf14b28b83a
kirang89 <me@kirang.in>
2016-02-28 22:33:56 +0530

    Add paren-face support for elisp files

--- kiran/init-elisp.el
+++ kiran/init-elisp.el
@@ -12,6 +12,8 @@
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'emacs-lisp-mode-hook (lambda () (rainbow-delimiters-mode)))
+(add-hook 'emacs-lisp-mode-hook 'paren-face-mode)
+
 
 (provide 'init-elisp)
 ;;; init-elisp ends here
