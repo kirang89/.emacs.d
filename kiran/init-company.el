62543e2440c3e44b7ae7d91be615c32a348601f1
kirang89 <me@kirang.in>
2016-05-18 10:15:28 +0530

    Setup company using use-package

--- kiran/init-company.el
+++ kiran/init-company.el
@@ -1,22 +1,38 @@
-(add-to-list 'load-path "/Users/kiran/.emacs.d/elpa/company-20150727.1415")
-(require 'company)
-(add-hook 'after-init-hook 'global-company-mode)
-
-(setq company-require-match nil
-      company-selection-wrap-around t
-      company-tooltip-align-annotations t
-      company-tooltip-limit 10)
+(use-package company
+  :ensure t
+  :diminish ""
+  :init
+  ;; (add-hook 'prog-mode-hook 'company-mode)
+  ;; (add-hook 'comint-mode-hook 'company-mode)
+  :config
+  (global-company-mode)
+  ;; Quick-help (popup documentation for suggestions).
+  ;; (use-package company-quickhelp
+  ;;   :if window-system
+  ;;   :init (company-quickhelp-mode 1))
+  ;; Company settings.
+  (setq company-tooltip-limit 20)
+  (setq company-idle-delay 0.2)
+  (setq company-echo-delay 0)
+  (setq company-minimum-prefix-length 2)
+  (setq company-require-match nil)
+  (setq company-selection-wrap-around t)
+  (setq company-tooltip-align-annotations t)
+  ;; weight by frequency
+  (setq company-transformers '(company-sort-by-occurrence))
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous))
 
 ;;(company-quickhelp-mode 1)
 
 ;; Decrease delay before autocompletion popup shows up
-(setq company-idle-delay 0.2)
+;; (setq company-idle-delay 0.2)
 
 ;; Remove blinking
-(setq company-echo-delay 0)
+;; (setq company-echo-delay 0)
 
-;; weight by frequency
-(setq company-transformers '(company-sort-by-occurrence))
 
 (provide 'init-company)
 ;;; init-company.el ends here
