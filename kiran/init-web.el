39bc9be99b125d3c9f59f26096ce7539e2fd01a7
kirang89 <me@kirang.in>
2016-06-01 02:26:02 +0530

    use-packageify all web-mode config

--- kiran/init-web.el
+++ kiran/init-web.el
@@ -13,12 +13,43 @@
 (setq web-mode-enable-auto-expanding t)
 (setq web-mode-enable-css-colorization t)
 
-;; Use M-x httpd-start to enable the web server
-;;(add-hook 'web-mode-hook 'impatient-mode)
+(use-package web-mode
+  :ensure t
+  :mode ("\\.html$" . web-mode))
 
-(require 'emmet-mode)
-(add-hook 'web-mode-hook 'emmet-mode)
-(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+(use-package web-beautify
+  :ensure t
+  :commands (web-beautify-css
+             web-beautify-css-buffer
+             web-beautify-html
+             web-beautify-html-buffer
+             web-beautify-js
+             web-beautify-js-buffer))
+
+;; (use-package js3-mode
+;;   :ensure t
+;;   :mode ("\\.js$" . js3-mode))
+
+(use-package impatient-mode
+  :ensure t
+  :diminish (impatient-mode . "ι")
+  :commands (impatient-mode))
+
+(use-package emmet-mode
+  :ensure t
+  :diminish (emmet-mode . "ε")
+  :bind* (("C-)" . emmet-next-edit-point)
+          ("C-(" . emmet-prev-edit-point))
+  :commands (emmet-mode
+             emmet-next-edit-point
+             emmet-prev-edit-point)
+  :config
+  (add-hook 'web-mode-hook 'emmet-mode)
+  ;; Auto-start on any markup modes
+  (add-hook 'sgml-mode-hook 'emmet-mode))
+
+(use-package nginx-mode
+  :commands (nginx-mode))
 
 (provide 'init-web)
 ;;; init-web.el ends here
