c9ba2af6765346d126f3f467d4d20758175fa66a
kirang89 <me@kirang.in>
2016-06-01 02:18:08 +0530

    Move minor mode into efuns

--- kiran/efuns.el
+++ kiran/efuns.el
@@ -289,5 +289,24 @@ C-u C-u COMMAND -> Open/switch to a scratch buffer in `emacs-elisp-mode'"
 
 (global-set-key (kbd "M-RET") 'kg/newline-for-code)
 
+;; Define minor mode to disable mouse interactions in emacs buffer
+;; Source: http://endlessparentheses.com/disable-mouse-only-inside-emacs.html
+(define-minor-mode disable-mouse-mode
+  "A minor-mode that disables all mouse keybinds."
+  :global t
+  :lighter " 🐭"
+  :keymap (make-sparse-keymap))
+
+(dolist (type '(mouse down-mouse drag-mouse
+                      double-mouse triple-mouse))
+  (dolist (prefix '("" C- M- S- M-S- C-M- C-S- C-M-S-))
+    ;; Yes, I actually HAD to go up to 7 here.
+    (dotimes (n 7)
+      (let ((k (format "%s%s-%s" prefix type n)))
+        (define-key disable-mouse-mode-map
+          (vector (intern k)) #'ignore)))))
+
+(add-hook 'after-init-hook (disable-mouse-mode 1))
+
 (provide 'efuns)
 ;;; efuns.el ends here
