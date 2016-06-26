3995ec889d11895c284affd44c18f9f6f5b71a34
kirang89 <me@kirang.in>
2016-06-26 19:26:38 +0530

    Setup neotree

--- init.el
+++ init.el
@@ -173,6 +173,17 @@
   (bind-key "}" #'paredit-close-curly json-mode-map))
 
 
+(use-package neotree
+  :init
+  (setq neo-smart-open t)
+  (setq-default neo-dont-be-alone t)
+  (setq neo-hidden-files-regexp
+        "^\\.\\|~$\\|^#.*#$\\|^target$\\|^pom\\.*\\|^__pycache__$\\|^env$")
+  :config
+  ;; (local-set-key (kbd "M-<up>") 'neotree-select-up-node)
+  ;; (local-set-key (kbd "M-<down>") 'neotree-select-down-node)
+  (global-set-key [f9] 'neotree-toggle))
+
 ;;; =====================================
 ;;; Experimental Stuff
 ;;; =====================================
