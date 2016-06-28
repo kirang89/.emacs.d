e629fb291c32a3250fe9271c1b8be19c0a3a7d4c
kirang89 <me@kirang.in>
2016-06-28 21:13:22 +0530

    Remove Marmalade from package repos

--- kiran/init-packages.el
+++ kiran/init-packages.el
@@ -1,8 +1,6 @@
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
 (package-initialize)
 
