7b8cf1de07ef2262611c320e7c1338047801b8ba
kirang89 <me@kirang.in>
2016-06-28 21:12:51 +0530

    Cleanup appearance config

--- kiran/init-looks.el
+++ kiran/init-looks.el
@@ -21,30 +21,11 @@
 
 ;; Good font sizes
 ;; ===============
-;;(set-frame-font "Code New Roman-16:spacing=m")
-;;(set-frame-font "Consolas-14:weight=bold:spacing=m")
-;;(set-frame-font "Inconsolata-13:weight=regular:spacing=m")
-;;(set-frame-font "Inconsolata-g-15:spacing=m")
-;;(set-frame-font "Source Code Pro-13.0:weight=light:spacing=m")
-;;(set-frame-font "Source Code Pro-13:weight=medium:height=150")
-;; with ls 0.2
-(set-frame-font "Hasklig-13:weight=medium:spacing=m") ;; with ls 0.2
-;;(set-frame-font "Menlo-13:weight=regular:spacing=m")
-;;(set-frame-font "Generic Mono II-14:weight=medium:spacing=m")
-;;(set-frame-font "Roboto Mono 13")
-;;(set-frame-font "Droid Sans Mono-13:weight=bold:spacing=m")
-;;(set-frame-font "Liberation Mono-13:weight=bold:spacing=p")
+(set-frame-font "Source Code Pro-15.0:weight=regular:spacing=m")
 ;;(set-frame-font "Office Code Pro 13")
-;;(set-default-font "-apple-Monaco-normal-normal-normal-*-13-*-*-*-*-0-iso10646-1")
-;;(set-frame-font "-*-Hack-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
-;;(set-frame-font "DejaVu Sans Mono-13:weight=bold:spacing=p")
-;;(set-frame-font "Fira Mono Medium for Powerline-15:spacing=m")
-;;(set-frame-font "NanumGothicCoding-17:spacing=m")
-;;(set-frame-font "AHAMONO-17:spacing=m")
-;;(set-frame-font "Anonymous Pro-14:spacing=m")
 
 ;; Set default line spacing (relative)
-(setq-default line-spacing 0.1)
+(setq-default line-spacing 0.3)
 
 ; don't show the tool bar
 (require 'tool-bar)
@@ -88,24 +69,22 @@
 ;; M-q should fill at 80 chars, not 70
 (setq-default fill-column 80)
 
-;; Show line numbers, dynamically with spaces on either side
-;; (defadvice linum-update-window (around linum-dynamic activate)
-;;   (let* ((w (length (number-to-string
-;;                      (count-lines (point-min) (point-max)))))
-;;          (linum-format (concat " %" (number-to-string w) "d ")))
-;;     ad-do-it))
+;;(setq linum-format "%5d ")
+;;(custom-set-variables '(linum-format 'dynamic))
 
-;;(setq linum-format " %4d ")
-(setq linum-format "%5i ")
+(defadvice linum-update-window (around linum-dynamic activate)
+  (let* ((w (length (number-to-string
+                     (count-lines (point-min) (point-max)))))
+         (linum-format (concat "  %" (number-to-string w) "d ")))
+    ad-do-it))
 
 (defun kg/reset-linum ()
-  "Reset formatting of line numbers"
+  "Reset formatting of line numbers."
   (interactive)
-  (let ((font "Source Code Pro-11.0:weight=regular:spacing=m"))
+  (let ((font "Source Code Pro-11:weight=regular:spacing=m"))
     (require 'linum)
     (set-face-attribute 'linum nil :font font)))
 
-(add-hook 'after-init-hook #'kg/reset-linum)
 (add-hook 'prog-mode-hook 'linum-mode)
 
 ;; set color for selection
@@ -117,6 +96,7 @@
       scroll-step 5
       scroll-conservatively 1000
       scroll-preserve-screen-position 1)
+(add-hook 'after-init-hook #'kg/reset-linum)
 
 ;; set unique names for two similar buffers
 (require 'uniquify)
