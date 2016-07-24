f4c0e293d41abf912fac75426d1e536b39693543
kirang89 <me@kirang.in>
2016-07-24 22:51:35 +0530

    Move certain settings from main init file

--- kiran/init-editing.el
+++ kiran/init-editing.el
@@ -1,3 +1,11 @@
+
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(prefer-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+
+(setq ns-command-modifier 'meta)
+
 ;; require final newlines in files when they are saved
 (setq require-final-newline t)
 
