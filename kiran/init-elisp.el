90953fb6368cf7863c5aae5625fc83beade644ee
kirang89 <me@kirang.in>
2016-08-03 05:54:23 +0530

    Misc changes to elisp configuration

--- kiran/init-elisp.el
+++ kiran/init-elisp.el
@@ -7,13 +7,18 @@
 ;;; Code:
 
 (add-hook 'emacs-lisp-mode-hook 'flycheck-mode)
+
 ;;(add-hook 'emacs-lisp-mode-hook 'linum-mode)
+
 ;; Documentation at point for lisp files
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
 
 (add-hook 'emacs-lisp-mode-hook (lambda () (rainbow-delimiters-mode)))
+
 (add-hook 'emacs-lisp-mode-hook 'paren-face-mode)
+
+(add-hook 'emacs-lisp-mode-hook 'aggressive-indent-mode)
 
 
 (provide 'init-elisp)
