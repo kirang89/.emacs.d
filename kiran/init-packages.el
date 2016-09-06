3d9ea38d20640e46202c0b1c4e104956cea4c170
kirang89 <me@kirang.in>
2016-09-06 17:42:14 +0530

    Fix issue with connecting to melpa via https

--- kiran/init-packages.el
+++ kiran/init-packages.el
@@ -1,6 +1,6 @@
 (require 'package)
 
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/"))
 
 (package-initialize)
