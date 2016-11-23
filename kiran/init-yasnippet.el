b16fc7043009db649c4ab040b3b2db4d4f0d512e
kirang89 <me@kirang.in>
2016-11-24 02:34:13 +0530

    Make changes to yasnippet configuration

--- kiran/init-yasnippet.el
+++ kiran/init-yasnippet.el
@@ -1,14 +1,10 @@
-
-;; yasnippet
-;;(add-to-list 'load-path "~/.emacs.d/elpa/yasnippet-20141005.124/")
-
-(setq yas-snippet-dirs '("~/.emacs.d/snippets"))
-;; (require 'yasnippet)
-;; (yas-global-mode 1)
-(autoload 'yasnippet "yasnippet" "Snippets for every occasion" t)
-(eval-after-load "yasnippet"
-  (lambda ()
-    (yas-global-mode 1)))
+(use-package yasnippet
+  :init
+  (add-hook 'clojure-mode-hook 'yas-minor-mode)
+  (add-hook 'js-mode-hook 'yas-minor-mode)
+  (add-hook 'python-mode-hook 'yas-minor-mode)
+  :config
+  (setq yas-snippet-dirs '("~/.emacs.d/snippets")))
 
 (provide 'init-yasnippet)
 ;;; init-yasnippet.el ends here
