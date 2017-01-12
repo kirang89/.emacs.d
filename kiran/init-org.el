beee775e5fa14384ff0c1a570b56bfd50974503c
kirang89 <me@kirang.in>
2017-01-12 15:35:47 +0400

    Move org-mode configuration to use-package

--- kiran/init-org.el
+++ kiran/init-org.el
@@ -1,47 +1,97 @@
+(use-package org
+  :defer t
+  :init
+  (setq org-directory "~/Box Sync/org-notes"
+        org-default-notes-file (concat org-directory "/notes.txt")
+        org-agenda-files '("~/Box Sync/org-notes/agenda.org")
+        ;; Prefer UTF-8
+        org-export-coding-system 'utf-8
+        org-startup-indented t
+        org-indent-mode t
+        ;; Make code pretty
+        org-src-fontify-natively t
+        ;; change behavior of TAB as per the appropriate major mode
+        org-src-tab-acts-natively t
+        ;; Preserve the indentation inside of source blocks
+        org-src-preserve-indentation t
+        ;; week starts on Monday
+        org-agenda-start-on-weekday 0
+        ;; Org-html export configuration
+        org-html-doctype "html5"
+        org-html-html5-fancy t
+        org-html-postamble nil
+        ;; Use speed commands like `t` to change todo status
+        org-use-speed-commands t
+        ;; Syntax highlighting for org-export
+        org-latex-listings 'minted
+        org-hide-emphasis-markers t
+        org-ellipsis "•••")
 
-;; org mode
-(require 'org)
+  ;; The directory where images should be downloaded to, when dragged into
+  ;; an org buffer
+  (setq-default org-download-image-dir "~/Box Sync/org-notes/resources")
 
-(setq org-directory "~/Box Sync/org-notes"
-      org-default-notes-file (concat org-directory "/notes.txt")
-      org-agenda-files '("~/Box Sync/org-notes/agenda.org")
-      ;; Prefer UTF-8
-      org-export-coding-system 'utf-8
-      org-startup-indented t
-      org-indent-mode t
-      ;; Make code pretty
-      org-src-fontify-natively t
-      ;; change behavior of TAB as per the appropriate major mode
-      org-src-tab-acts-natively t
-      ;; Preserve the indentation inside of source blocks
-      org-src-preserve-indentation t
-      ;; week starts on Monday
-      org-agenda-start-on-weekday 0
-      ;; Org-html export configuration
-      org-html-doctype "html5"
-      org-html-html5-fancy t
-      org-html-postamble nil
-      ;; Use speed commands like `t` to change todo status
-      org-use-speed-commands t
-      ;; Syntax highlighting for org-export
-      org-latex-listings 'minted
-      org-hide-emphasis-markers t
-      org-ellipsis "•••")
-
-;; The directory where images should be downloaded to, when dragged into
-;; an org buffer
-(setq-default org-download-image-dir "~/Box Sync/org-notes/resources")
-
-(setq org-todo-keyword-faces
+  (setq org-todo-keyword-faces
         '(("TODO" :foreground "red" :weight bold)
           ("DOING" :foreground "magenta" :weight bold)
           ("DONE" :foreground "green" :weight bold)))
 
-;; Better bullets
-(font-lock-add-keywords 'org-mode
-                        '(("^ +\\([-*]\\) "
-                           (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+  ;; Better bullets
+  (font-lock-add-keywords 'org-mode
+                          '(("^ +\\([-*]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 
+  (setq TeX-view-program-list
+      '(("Preview.app" "open -a Preview.app %o")
+        ("Skim" "open -a Skim.app %o")
+        ("displayline" "/Applications/Skim.app/Contents/SharedSupport/displayline -b %n %o %b")
+        ("open" "open %o"))
+      ;; Select the viewers for each file type.
+      TeX-view-program-selection
+      '((output-dvi "open")
+        (output-pdf "displayline")
+        (output-html "open")))
+
+  (setq org-babel-sh-command "bash")
+
+  :config
+  (global-set-key (kbd "C-x c") 'org-capture)
+
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+  (add-hook 'org-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-x p")
+                           'org-mac-chrome-insert-frontmost-url)))
+
+  ;; Use unicode chars instead of regular bullets
+  (use-package org-bullets
+    :defer t
+    :commands org-bullets-mode
+    ;; :config (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+    )
+
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((python . t)
+     (clojure . t)
+     (sh . t)
+     ;; (latex . t)
+     ))
+
+
+  (use-package ox-latex
+    :defer t
+    :init
+    (add-to-list 'org-latex-packages-alist '("" "minted"))
+    (setq org-latex-pdf-process
+      '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f")))
+
+  (use-package ox-md
+    :defer t))
+
+;; ===========
 
 (defun kg/grep-notes ()
   "Search my notes."
@@ -70,14 +120,6 @@
 ;;         ))
 
 ;;(global-set-key (kbd "C-c C-a") 'org-agenda)
-(global-set-key (kbd "C-x c") 'org-capture)
-
-;; load org-mode markdown export
-(eval-after-load "org"
-  '(require 'ox-md nil t))
-
-;; Spellcheck my org mode files.
-;;(add-hook 'org-mode-hook 'flyspell-mode)
 
 (defun org-archive-tasks ()
   "Archive DONE tasks in org-mode."
@@ -108,17 +150,6 @@
 (setq org-html-head-extra (concat html-export-headers
                                   html-export-styles))
 
-;; Activate babel support for specified languages
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((python . t)
-   (clojure . t)
-   (sh . t)
-   ;; (latex . t)
-   ))
-
-(setq org-babel-sh-command "bash")
-
 ;; (use-package ob-clojure
 ;;   :init
 ;;   (setq org-babel-clojure-backend 'cider)
@@ -126,38 +157,6 @@
 
 ;; don't run stuff automatically on export
 ;;(setq org-export-babel-evaluate nil)
-
-;; Use unicode chars instead of regular bullets
-;; (use-package org-bullets
-;;   :ensure t
-;;   :commands org-bullets-mode
-;;   ;; :config (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-;;   )
-
-;; (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-(add-hook 'org-mode-hook
-          (lambda ()
-            (local-set-key (kbd "C-x p")
-                           'org-mac-chrome-insert-frontmost-url)))
-
-;; Org latex configuration
-(require 'ox-latex)
-(add-to-list 'org-latex-packages-alist '("" "minted"))
-(setq org-latex-pdf-process
-      '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
-
-;; Deft configuration
-;; (use-package deft
-;;   :bind ("C-x C-g" . deft-find-file)
-;;   :config
-;;   (setq deft-extensions '("txt" "md" "org")
-;;         deft-directory "~/Box Sync/org-notes/"
-;;         deft-recursive t
-;;         deft-use-filename-as-title t
-;;         deft-strip-summary-regexp "\\*"
-;;         deft-auto-save-interval 60))
 
 ;; (defface org-block-begin-line
 ;;   '((t (:underline "#8b5a2b" :foreground "#8b7355" :background "#8b7355")))
