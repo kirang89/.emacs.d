7403ed2dd667c98019429b10d9107bd772dfea1d
kirang89 <me@kirang.in>
2017-01-12 15:34:45 +0400

    Move helm to use-package

--- kiran/init-helm.el
+++ kiran/init-helm.el
@@ -3,6 +3,29 @@
 ;;(run-with-idle-timer 1 nil (lambda () (helm-mode 1)))
 
 (use-package helm
+  :defer t
+  :init
+  ;; Make Helm look nice.
+  (setq-default helm-M-x-fuzzy-match t
+                helm-buffers-fuzzy-matching t
+                helm-recentf-fuzzy-match t
+                helm-apropos-fuzzy-match t
+                ;; open Helm window in the current window where point is in
+                helm-split-window-in-side-p t
+                helm-ff-ido-style-backspace 'always)
+
+  ;;(set-face-attribute 'helm-source-header nil :height 0.75)
+
+  ;; Replace common selectors with Helm versions.
+  ;; (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+  (global-set-key (kbd "C-x C-f") #'helm-find-files)
+  (global-set-key (kbd "C-x b") #'helm-mini)
+  (global-set-key (kbd "M-x") #'helm-M-x)
+  (global-set-key (kbd "M-y") #'helm-show-kill-ring)
+  (global-set-key (kbd "C-x C-r") #'helm-recentf)
+  (global-set-key (kbd "C-x C-o") #'helm-occur)
+  (global-set-key (kbd "C-c C-s") #'helm-do-grep-ag)
+
   :config
   (require 'helm-config)
   (require 'helm)
@@ -19,33 +42,12 @@
   ;;            ("C-x C-g" . helm-projectile-grep))
   ;;     :config (helm-projectile-on)))
 
-  ;; Make Helm look nice.
-  (setq-default helm-M-x-fuzzy-match t
-		helm-buffers-fuzzy-matching t
-		helm-recentf-fuzzy-match t
-		helm-apropos-fuzzy-match t
-		;; open Helm window in the current window where point is in
-		helm-split-window-in-side-p t
-        helm-ff-ido-style-backspace 'always)
-
-  ;;(set-face-attribute 'helm-source-header nil :height 0.75)
-
-  ;; Replace common selectors with Helm versions.
-  ;; (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
-  ;; (global-set-key (kbd "C-x C-f") #'helm-find-files)
-  (global-set-key (kbd "C-x b") #'helm-mini)
-  (global-set-key (kbd "M-x") #'helm-M-x)
-  (global-set-key (kbd "M-y") #'helm-show-kill-ring)
-  (global-set-key (kbd "C-x C-r") #'helm-recentf)
-  (global-set-key (kbd "C-x C-o") #'helm-occur)
-  (global-set-key (kbd "C-c C-s") #'helm-do-grep-ag)
-
   (with-eval-after-load 'helm-files
     (define-key helm-read-file-map (kbd "<backspace>") 'helm-find-files-up-one-level)
     (define-key helm-find-files-map (kbd "<backspace>") 'helm-find-files-up-one-level)))
 
 ;; (use-package helm-ls-git
-;;   :ensure t
+;;   :defer t
 ;;   :bind ("C-x C-d" . helm-browse-project))
 
 (provide 'init-helm)
