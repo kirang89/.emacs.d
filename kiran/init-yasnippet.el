cfffa40a285fa841c35244a00881a11da2b510f4
kirang89 <me@kirang.in>
2017-02-10 20:27:44 +0530

    Yasnippet all the time

--- kiran/init-yasnippet.el
+++ kiran/init-yasnippet.el
@@ -1,10 +1,9 @@
 (use-package yasnippet
-  :defer t
+  :init
+  (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
+
   :config
-  (add-hook 'clojure-mode-hook 'yas-minor-mode)
-  ;;(add-hook 'js-mode-hook 'yas-minor-mode)
-  (add-hook 'python-mode-hook 'yas-minor-mode)
-  (setq yas-snippet-dirs '("~/.emacs.d/snippets")))
+   (yas-global-mode))
 
 (provide 'init-yasnippet)
 ;;; init-yasnippet.el ends here
