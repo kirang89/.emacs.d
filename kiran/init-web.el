00f2f6e6f2a2e2fdff3b2da8af496d17dff5a858
kirang89 <me@kirang.in>
2017-04-13 18:02:46 +0530

    Setup emmet mode for web dev

--- kiran/init-web.el
+++ kiran/init-web.el
@@ -41,9 +41,8 @@
              emmet-prev-edit-point)
   :config
   ;; Auto-start on any markup modes
-  (add-hook 'sgml-mode-hook 'emmet-mode))
-
-(add-hook 'web-mode-hook 'emmet-mode)
+  (add-hook 'sgml-mode-hook 'emmet-mode)
+  (add-hook 'web-mode-hook 'emmet-mode))
 
 (use-package nginx-mode
   :commands (nginx-mode))
