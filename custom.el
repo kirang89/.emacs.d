9cc193cacc7bd8970a6d7098eb34cd3c6aa7bfe7
Kiran Gangadharan <me@kirang.in>
2017-06-18 03:21:21 +0530

    Changes to custom.el

--- custom.el
+++ custom.el
@@ -5,13 +5,17 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes (quote (railscasts-reloaded)))
+ '(ansi-term-color-vector
+   [unspecified "#1B2B34" "#EC5f67" "#99C794" "#FAC863" "#6699CC" "#C594C5" "#6699CC" "#C0C5CE"] t)
+ '(custom-enabled-themes (quote (dracula)))
  '(custom-safe-themes
    (quote
-    ("cd0d4fdf764f757fd659ee2697239a62f38d15203000ced1ad8e43c978942c68" default)))
+    ("50e7f9d112e821e42bd2b8410d50de966c35c7434dec12ddea99cb05dd368dd8" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" "e1551b5516e0a439b6ab019ba00cee866e735f66f22ff67a5d882ad0f1383454" "f641bdb1b534a06baa5e05ffdb5039fb265fde2764fbfd9a90b0d23b75f3936b" "c3c0a3702e1d6c0373a0f6a557788dfd49ec9e66e753fb24493579859c8e95ab" "cd0d4fdf764f757fd659ee2697239a62f38d15203000ced1ad8e43c978942c68" default)))
+ '(electric-pair-mode t)
+ '(fci-rule-color "#383838")
  '(package-selected-packages
    (quote
-    (pip-requirements org-mac-link lispy dired window-numbering visual-regexp-steroids counsel dracula-theme railscasts-reloaded-theme magit exec-path-from-shell org-bullets clojure-mode-extra-font-locking clj-refactor clojars sphinx-doc elpy ido-vertical-mode flx-ido flycheck paredit company-web company-statistics company-jedi rainbow-delimiters key-chord-mode company helm-ag ag paren-face aggressive-indent multiple-cursors buffer-move avy projectile expand-region paradox key-chord use-package ivy))))
+    (discover-my-major json-mode nginx-mode emmet-mode web-beautify web-mode ox-md ox-latex smartparens pip-requirements lispy dired window-numbering visual-regexp-steroids counsel dracula-theme railscasts-reloaded-theme magit exec-path-from-shell org-bullets clojure-mode-extra-font-locking clj-refactor clojars sphinx-doc elpy ido-vertical-mode flx-ido flycheck paredit company-web company-statistics company-jedi rainbow-delimiters key-chord-mode company helm-ag ag paren-face aggressive-indent multiple-cursors buffer-move avy projectile expand-region paradox key-chord use-package ivy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
