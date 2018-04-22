e2417b6ed94b29a00f85ae1cf6968c51d03995b7
Kiran Gangadharan <me@kirang.in>
2018-04-22 20:40:29 +0530

    Some modeline changes

--- kiran/init-modeline.el
+++ kiran/init-modeline.el
@@ -1,6 +1,6 @@
-;; (line-number-mode t)
-;; (column-number-mode -1)
-;; (size-indication-mode -1)
+(line-number-mode t)
+(column-number-mode -1)
+(size-indication-mode -1)
 
 ;; Fix for broken seperator colors on the modeline
 ;;(setq ns-use-srgb-colorspace nil)
@@ -13,10 +13,6 @@
 ;;     (setq spaceline-highlight-face-func 'spaceline-highlight-face-modified)
 ;;     (spaceline-toggle-hud-on)
 ;;     (spaceline-emacs-theme)))
-
-;; (require 'spaceline-all-the-icons)
-;; (spaceline-all-the-icons-theme)
-
 
 ;; Set default font for different modeline modes
 ;; (defun kg/set-modeline-face ()
@@ -44,8 +40,7 @@
   (eval-after-load "yasnippet" '(diminish 'yas-minor-mode))
   (eval-after-load "eldoc" '(diminish 'eldoc-mode))
   (eval-after-load "company" '(diminish 'company-mode))
-  (eval-after-load "projectile" '(diminish 'projectile-mode))
-  ;; (eval-after-load "flyspell" '(diminish 'flyspell-mode))
+  ;; (eval-after-load "projectile" '(diminish 'projectile-mode))
   (eval-after-load "sphinx-doc" '(diminish 'sphinx-doc-mode))
   (eval-after-load "org-indent" '(diminish 'org-indent-mode))
   (eval-after-load "smartparens" '(diminish 'smartparens-mode))
@@ -58,7 +53,7 @@
   (eval-after-load "outline" '(diminish 'outline-minor-mode))
   (eval-after-load "ivy" '(diminish 'ivy-mode))
   (eval-after-load "google-this" '(diminish 'google-this-mode))
-  (eval-after-load "aggressive-indent" '(diminish 'aggressive-indent-mode "Ai"))
+  (eval-after-load "aggressive-indent" '(diminish 'aggressive-indent-mode))
   (eval-after-load "clj-refactor" '(diminish 'clj-refactor-mode))
   (eval-after-load "highlight-indentation" '(diminish 'highlight-indentation-mode)))
 
@@ -82,15 +77,15 @@ want to use in the modeline *in lieu of* the original.")
   (interactive)
   (loop for cleaner in mode-line-cleaner-alist
         do (let* ((mode (car cleaner))
-                 (mode-str (cdr cleaner))
-                 (old-mode-str (cdr (assq mode minor-mode-alist))))
+                  (mode-str (cdr cleaner))
+                  (old-mode-str (cdr (assq mode minor-mode-alist))))
              (when old-mode-str
-                 (setcar old-mode-str mode-str))
-               ;; major mode
+               (setcar old-mode-str mode-str))
+             ;; major mode
              (when (eq mode major-mode)
                (setq mode-name mode-str)))))
 
-(add-hook 'after-change-major-mode-hook 'clean-mode-line)
+;; (add-hook 'after-change-major-mode-hook 'clean-mode-line)
 
 
 ;;=========
