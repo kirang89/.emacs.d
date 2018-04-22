fa50bea91e485b305acbb03bcddf2135ea2a9ef4
Kiran Gangadharan <me@kirang.in>
2018-04-22 20:17:03 +0530

    Refactor clojure configuration

--- kiran/init-clojure.el
+++ kiran/init-clojure.el
@@ -1,6 +1,4 @@
 ;; A little more syntax highlighting
-;;(require 'clojure-mode-extra-font-locking)
-
 (use-package clojure-mode-extra-font-locking
   :ensure t)
 
@@ -14,35 +12,43 @@
              '(("(\\(facts?\\)"
                 (1 font-lock-keyword-face))
                ("(\\(background?\\)"
-                (1 font-lock-keyword-face))))
+                (1 font-lock-keyword-face))))))
 
-            (define-clojure-indent
-              (defroutes 'defun)
-              (fact 1)
-              (facts 1)
-              (clojure.spec/fdef 1)
-              (GET 2)
-              (POST 2)
-              (PUT 2)
-              (DELETE 2)
-              (HEAD 2)
-              (ANY 2)
-              (context 2))))
+(eval-after-load 'clojure-mode
+  '(progn
+     (define-key clojure-mode-map (kbd "C-c C-h") #'clojure-cheatsheet)))
 
-(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'clojure-mode-hook 'kg/set-pretty-symbols)
-(add-hook 'clojure-mode-hook 'paren-face-mode)
-(add-hook 'clojure-mode-hook 'paredit-mode)
-(add-hook 'clojure-mode-hook 'subword-mode)
+(define-clojure-indent
+  (defroutes 'defun)
+  (fact 1)
+  (facts 1)
+  (s/fdef 1)
+  (spec/fdef 1)
+  (GET 2)
+  (POST 2)
+  (PUT 2)
+  (DELETE 2)
+  (HEAD 2)
+  (ANY 2)
+  (context 2))
+
+(require 'paredit)
+(add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'clojure-mode-hook #'paredit-mode)
+(add-hook 'clojure-mode-hook #'subword-mode)
 (add-hook 'clojure-mode-hook
           (lambda ()
-            (local-set-key (kbd "C-c r") 'cider-eval-region)
-            (local-set-key (kbd "C-c C-s") 'paredit-wrap-round)
-            (local-set-key (kbd "<C-return>") 'cider-eval-last-sexp)
             (local-set-key (kbd "C-c d c") 'clojure-cheatsheet)
-            (local-set-key (kbd "C-'") 'cider-debug-defun-at-point)
-            (local-set-key (kbd "C-M-d") 'cider-debug-defun-at-point)))
+            (local-set-key (kbd "<C-right>") #'paredit-forward-slurp-sexp)
+            (local-set-key (kbd "<C-left>") #'paredit-forward-barf-sexp)))
 
+;; Clojurescript hooks
+(add-hook 'clojurescript-mode-hook #'paredit-mode)
+(add-hook 'clojurescript-mode-hook #'subword-mode)
+(add-hook 'clojurescript-mode-hook
+          (lambda ()
+            (local-set-key (kbd "<C-right>") #'paredit-forward-slurp-sexp)
+            (local-set-key (kbd "<C-left>") #'paredit-forward-barf-sexp)))
 
 ;;;;
 ;; Cider
@@ -54,6 +60,7 @@
 
   (setq cider-repl-pop-to-buffer-on-connect t
         cider-mode-line nil
+        cider-prompt-for-symbol nil
         cider-show-error-buffer t
         cider-auto-select-error-buffer t
         cider-repl-history-file "~/.emacs.d/cider-history"
@@ -63,73 +70,51 @@
         cider-docview-fill-column 70
         cider-stacktrace-fill-column 76
         nrepl-hide-special-buffers t
-        ;; Stop error buffer from popping up while working in buffers other than REPL:
         nrepl-popup-stacktraces nil
         nrepl-log-messages nil
         nrepl-hide-special-buffers t
         cider-repl-use-pretty-printing t
         cider-repl-display-help-banner nil
         cider-repl-result-prefix ";; => "
-        cider-show-error-buffer nil)
+        cider-show-error-buffer nil
+        ;; highlight result of evaluating clojure code
+        cider-overlays-use-font-lock t
+        cider-cljs-lein-repl
+        "(do (require 'figwheel-sidecar.repl-api)
+           (figwheel-sidecar.repl-api/start-figwheel!)
+           (figwheel-sidecar.repl-api/cljs-repl))"
+        ;; experimental
+        cider-font-lock-dynamically '(macro core function var))
 
   :config
 
+  (add-hook 'clojure-mode-hook
+            (lambda ()
+              (local-set-key (kbd "<C-return>") 'cider-eval-last-sexp)
+              (local-set-key (kbd "C-M-d") 'cider-debug-defun-at-point)))
   (add-hook 'cider-mode-hook #'eldoc-mode)
-  (add-hook 'cider-repl-mode-hook #'paredit-mode)
-  (add-hook 'cider-repl-mode-hook #'paren-face-mode)
-  (add-hook 'cider-repl-mode-hook #'subword-mode)
-  (add-hook 'cider-repl-mode-hook #'company-mode)
+  (add-hook 'cider-mode-hook #'paredit-mode)
+  (add-hook 'cider-mode-hook #'subword-mode)
   (add-hook 'cider-mode-hook #'company-mode)
-  ;; (add-hook 'cider-mode-hook
-  ;;           (local-set-key (kbd "C-c d") 'cider-debug-defun-at-point))
-  (add-hook 'cider-mode-hook
-            (local-set-key (kbd "<C-return>") 'cider-eval-defun-at-point))
 
-  (defun cider-send-and-evaluate-sexp ()
-    "Send and eval expression in REPL.
-
-Sends the s-expression located before the point or the active
-region to the REPL and evaluates it.Then the Clojure buffer is
-activated as if nothing happened."
-    (interactive)
-    (if (not (region-active-p))
-        (cider-insert-last-sexp-in-repl)
-      (cider-insert-in-repl
-       (buffer-substring (region-beginning) (region-end)) nil))
-    (cider-switch-to-repl-buffer)
-    (cider-repl-closing-return)
-    (cider-switch-to-last-clojure-buffer)
-    (message ""))
-
-  (bind-key "C-c C-v" 'cider-send-and-evaluate-sexp))
-
+  (add-hook 'cider-repl-mode-hook #'eldoc-mode)
+  (remove-hook 'cider-repl-mode-hook #'aggressive-indent-mode)
+  (add-hook 'cider-repl-mode-hook
+            (lambda ()
+              (local-set-key (kbd "C-l") 'cider-repl-clear-buffer))))
 
 (use-package clj-refactor
   :ensure t
+  :diminish ""
   :config
   (add-hook 'clojure-mode-hook
             (lambda ()
               (clj-refactor-mode 1)
+              (cljr-add-keybindings-with-prefix "C-c .")))
+  (add-hook 'clojurescript-mode-hook
+            (lambda ()
+              (clj-refactor-mode 1)
               (cljr-add-keybindings-with-prefix "C-c ."))))
-
-
-;; Look up packages in Clojars
-(use-package clojars
-  :ensure t)
-
-
-(defun kg/clj-src-file-name-from-test (name)
-  (s-with name
-    (s-replace "/test/" "/src/")
-    (s-replace "_test.clj" ".clj")))
-
-
-(defun kg/clj-test-file-name-from-src (name)
-  (interactive)
-  (s-with name
-    (s-replace "/src/" "/test/")
-    (s-replace ".clj" "_test.clj")))
-
 
 (defun kg/set-pretty-symbols ()
   "Make some word or string show as pretty Unicode symbols."
@@ -141,25 +126,21 @@ activated as if nothing happened."
           ("map" . 8614)    ; ↦
           )))
 
-
-(defun kg/helm-clojure-headlines ()
-  "Display headlines for the current Clojure file."
-  (interactive)
-  (helm-mode t)
-  (helm :sources '(((name . "Clojure Headlines")
-                    (volatile)
-                    (headline "^[;(]")))))
-
-
 (defun kg/ns-reset ()
   "Refreshes the cider repl."
   (interactive)
   (save-some-buffers)
   (cider-interactive-eval "(user/reset)"))
 
-
-;; TODO Setup flycheck using the below link
-;; https://github.com/howardabrams/dot-files/blob/master/emacs-clojure.org#linting
+(defun kg/cider-figwheel-repl ()
+  (interactive)
+  (save-some-buffers)
+  (with-current-buffer (cider-current-repl-buffer)
+    (goto-char (point-max))
+    (insert "(require 'figwheel-sidecar.repl-api)
+             (figwheel-sidecar.repl-api/start-figwheel!) ; idempotent
+             (figwheel-sidecar.repl-api/cljs-repl)")
+    (cider-repl-return)))
 
 (provide 'init-clojure)
 ;; init-clojure.el ends here.
