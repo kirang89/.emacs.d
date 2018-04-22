54944da9ba6ef2c3ac769f683d0a64937c0092ef
Kiran Gangadharan <me@kirang.in>
2018-04-22 20:28:46 +0530

    Refactor paredit configuration

--- kiran/init-parens.el
+++ kiran/init-parens.el
@@ -2,7 +2,7 @@
 ;; highlight matching parentheses
 (require 'paren)
 (show-paren-mode 1)
-(setq show-paren-delay 1)
+(setq show-paren-delay 0)
 (set-face-background 'show-paren-match (face-background 'default))
 (if (eq (frame-parameter nil 'background-mode) 'dark)
     (set-face-foreground 'show-paren-match "red")
@@ -27,21 +27,17 @@
   :init (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
 
 ;; Checkout http://danmidwood.com/content/2014/11/21/animated-paredit.html
-;; (use-package paredit
-;;   :ensure t
-;;   :init
-;;   (add-hook 'clojure-mode-hook 'enable-paredit-mode)
-;;   (add-hook 'cider-repl-mode-hook 'enable-paredit-mode)
-;;   (add-hook 'lisp-mode-hook 'enable-paredit-mode)
-;;   (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
-;;   (add-hook 'ielm-mode-hook 'enable-paredit-mode)
-;;   (add-hook 'json-mode-hook 'enable-paredit-mode))
+(use-package paredit
+  :ensure t
+  :init
+  (add-hook 'clojure-mode-hook 'enable-paredit-mode)
+  (add-hook 'cider-repl-mode-hook 'enable-paredit-mode)
+  (add-hook 'lisp-mode-hook 'enable-paredit-mode)
+  (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+  :config
+  (local-set-key (kbd "<C-right>") 'paredit-forward-slurp-sexp))
 
-;; (add-hook 'paredit-mode-hook
-;;           (lambda ()
-;;             (local-set-key (kbd "C-c C-s") 'paredit-wrap-round)
-;;             (local-set-key (kbd "M-n") 'paredit-splice-sexp-killing-forward)
-;;             (local-set-key (kbd "M-p") 'paredit-splice-sexp-killing-backward)))
+
 
 (provide 'init-parens)
 ;;; init-parens ends here
