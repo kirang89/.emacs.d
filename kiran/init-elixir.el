4f0c223cf96da35f6e90772dfbf3e6e440449d30
Kiran Gangadharan <me@kirang.in>
2018-04-22 20:16:07 +0530

    Add elixir configuration

--- kiran/init-elixir.el
+++ kiran/init-elixir.el
@@ -1,11 +1,19 @@
-(require 'elixir-format)
+(use-package alchemist
+  :init
+  (setq elixir-format-mix-path "/usr/local/bin/mix")
+  (setq alchemist-iex-program-name "/usr/local/bin/iex")
+  (setq alchemist-execute-command "/usr/local/bin/elixir")
+  (setq alchemist-test-status-modeline nil)
+  (setq alchemist-test-display-compilation-output t)
+  ;; (setq alchemist-hooks-compile-on-save t)
+  :config
+   (add-hook 'elixir-mode-hook 'alchemist-mode))
 
-(setq elixir-format-mix-path "/usr/local/bin/mix")
-
-(add-hook 'elixir-mode-hook
-          (lambda ()
-            (local-set-key (kbd "C-M-;") #'elixir-format)
-            (add-hook 'before-save-hook #'elixir-format-before-save)))
+;; (add-hook 'elixir-mode-hook
+;;           (lambda ()
+;;             (local-set-key (kbd "C-M-;") #'elixir-format)
+;;             ;; (global-unset-key (kbd "M -"))
+;;             (add-hook 'before-save-hook #'elixir-format-before-save)))
 
 (provide 'init-elixir)
 ;; init-clojure.el ends here.
