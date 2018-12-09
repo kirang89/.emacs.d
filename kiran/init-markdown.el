d75057452b705e81dfcb68c4d91974f5edfb15c0
Kiran Gangadharan <me@kirang.in>
2018-12-09 14:04:33 +0530

    Remove unnecessary configuration files

--- kiran/init-markdown.el
+++
@@ -1,7 +0,0 @@
-;; markdown-mode
-(autoload 'markdown-mode "markdown-mode" "Major mode for markdown editing" t)
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-
-(provide 'init-markdown)
-;;; init-markdown.el ends here
